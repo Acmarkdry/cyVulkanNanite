98485a86bde305ce0689837f003a3d9a24528447
acmarkdry <acmarkdry@163.com>
2025-11-15 11:33:28 +0800

    feat: set up render graph test environment

---
+++ src/RenderGraph/main.cpp
@@ -0,0 +1,20 @@
+// src/RenderGraph/main.cpp
+#include <iostream>
+
+// 包含你的 RenderGraph 头文件
+// #include "RenderGraph.h"
+
+int main(int argc, char* argv[])
+{
+    std::cout << "RenderGraph Test Started\n";
+    
+    // TODO: 在这里添加你的测试代码
+    // 例如：
+    // RenderGraph graph;
+    // graph.addPass(...);
+    // graph.compile();
+    // graph.execute();
+    
+    std::cout << "RenderGraph Test Completed\n";
+    return 0;
+}
