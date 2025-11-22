58692e638c11fb1dcf651937aa13777beeded6e1
acmarkdry <acmarkdry@163.com>
2025-11-22 16:49:51 +0800

    feat: develop render graph core functionality

---
+++ src/RenderGraph/ImageView.h
@@ -0,0 +1,20 @@
+#pragma once
+#include <vulkan/vulkan.hpp>
+
+namespace vk
+{
+
+}
+
+namespace cyRenderGraph
+{
+	class ImageView
+	{
+	public:
+	
+	private:
+		vk::UniqueImageView imageView;
+		
+	
+	};
+}
---
+++ src/RenderGraph/RenderPassCache.h
@@ -0,0 +1,25 @@
+#pragma once
+#include <algorithm>
+
+namespace cyRenderGraph
+{
+	
+	struct FrameBufferKey
+	{
+		FrameBufferKey()
+		{
+			std::fill();
+		}
+		std::array<const >
+	};
+	
+	class RenderPassCache
+	{
+	public:
+	
+	private:
+		
+	
+	};
+	
+}
