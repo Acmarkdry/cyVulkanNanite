b1214eed5735d97bbf1cf86b01da9f996c1d0cf5
acmarkdry <acmarkdry@163.com>
2025-12-14 00:08:46 +0800

    #优化singleton类

--- base/InstanceBase.h
+++ base/InstanceBase.h
@@ -1,25 +1,26 @@
 #pragma once
-#include <mutex>
-#include <memory>
 
 template<typename T>
-class Singleton {
+class Singleton
+{
 public:
-	// 获取单例实例
-	static T& getInstance() {
-		static T instance;  // C++11 保证线程安全
+	static T* getManager()
+	{
+		if (instance == nullptr)
+			instance = new T();
 		return instance;
 	}
+	static void destroy()
+	{
+		
+	}
 
-	// 禁止拷贝
-	Singleton(const Singleton&) = delete;
-	Singleton& operator=(const Singleton&) = delete;
-
-	// 禁止移动
+	Singleton(Singleton const&) = delete;
+	Singleton& operator=(Singleton const&) = delete;
 	Singleton(Singleton&&) = delete;
 	Singleton& operator=(Singleton&&) = delete;
-
-protected:
 	Singleton() = default;
-	virtual ~Singleton() = default;
+
+private:
+	static T* instance;
 };
