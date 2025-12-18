564025e34997953d112b332ce3931c03af3e1ff1
zhaochengyuan <55831520+Acmarkdry@users.noreply.github.com>
2025-12-18 21:59:47 +0800

    cmake添加第三方依赖

---
+++ src/NaniteMesh/NaniteMesh.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <OpenMesh/Core/IO/MeshIO.hh>
+#include <OpenMesh/Core/Mesh/TriMesh_ArrayKernelT.hh>
+#include <tinygltf/tiny_gltf.h>
+
+namespace Mesh 
+{
+	class NaniteMesh 
+	{
+	public:
+		
+	};
+}
